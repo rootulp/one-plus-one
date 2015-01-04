48f84937b5008ba0534e65a0388557eb06f8fe7e
Rootul Patel <rootulp@gmail.com>
2015-01-04 17:07:43 -0500

    change members back to people, silly mistake

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ActiveRecord::Base
   belongs_to :organization
   has_many :memberships
-  has_many :members, :through => :memberships
+  has_many :people, :through => :memberships
 
   validates :name, presence: true
 end
