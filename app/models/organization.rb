5136771c368b3c9f991faef98d67e5c2db5264f8
Rootul Patel <rootulp@gmail.com>
2015-01-04 17:08:36 -0500

    extremely basic pairing algorithm

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,29 @@ class Organization < ActiveRecord::Base
   has_many :teams
 
   validates :name, presence: true
+
+  def weekly_pairing
+    results = []
+    
+    while next_unpaired
+      person1 = next_unpaired
+       
+      if person1.find_pair
+        person2 = person1.find_pair
+        person1.update(paired: true)
+        person2.update(paired: true)
+        results << [person1.name, person2.name]
+      else
+        person1.update(paired: true)
+        results << [person1.name, "NO MATCH"]
+      end
+    end
+    
+    results
+  end
+
+  def next_unpaired
+    Person.find_by(paired: false)
+  end
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,22 @@ class Person < ActiveRecord::Base
 
   validates :email, uniqueness: { case_sensitive: false }
   validates :name, presence: true
+
+  def unmatched_teammates
+    teammates = []
+    self.teams.each do |team|
+      team.people.each do |member|
+        if member != self && member.paired == false
+          teammates << member 
+        end
+      end
+    end
+    teammates
+  end
+
+  def find_pair
+    return false if self.unmatched_teammates.empty?
+    self.unmatched_teammates.sample
+  end
+
 end
