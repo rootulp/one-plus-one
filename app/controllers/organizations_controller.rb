cc066c1307a3a5a988ff088a2e91a25b2b4add88
Rootul Patel <rootulp@gmail.com>
2015-01-04 19:21:55 -0500

    show weekly pairing on homepage

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,11 @@
 class OrganizationsController < ApplicationController
+  before_action :set_organization, only: [:show]
 
   def show
   end
 
+  private
+    def set_organization
+      @organization = Organization.first
+    end
 end
