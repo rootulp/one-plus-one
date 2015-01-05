d6ca8b7e8b699eb881da3f0fb31db79655015a67
Rootul Patel <rootulp@gmail.com>
2015-01-04 21:43:09 -0500

    membership specs

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,8 @@ class Membership < ActiveRecord::Base
   belongs_to :team
   belongs_to :person
 
+  validates :team, presence: true
+  validates :person, presence: true
   validates_associated :team
   validates_associated :person
   validates_uniqueness_of :person_id, :scope => :team_id
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Membership, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#new" do
+
+    context 'with invalid parameters' do
+      it "doesn't create anything" do
+        #Setup
+        Membership.create()
+        
+        #Expectation
+        expect(Membership.all.size).to eq(0)
+      end
+    end
+
+    context "with valid parameters" do
+      it "creates a membership" do
+        #Setup
+        Person.create( name: "Bob", email: "Bob@gmail.com" )
+        Team.create( name: "Builders" )
+        Membership.create( team: Team.find_by(name: "Builders"), person: Person.find_by(name: "Bob"))
+
+        #Expectation
+        expect(Membership.where(team_id: 1, person_id: 1)).to be_truthy
+      end
+    end
+
+    context 'with duplicate membership creations' do
+      it "only creates one" do
+        #Setup
+        Person.create( name: "Bob", email: "Bob@gmail.com" )
+        Team.create( name: "Builders" )
+        Membership.create( team: Team.find_by(name: "Builders"), person: Person.find_by(name: "Bob"))
+        Membership.create( team: Team.find_by(name: "Builders"), person: Person.find_by(name: "Bob"))
+        
+        #Expectation
+        expect(Membership.all.size).to eq(1)
+      end
+    end
+
+  end
+
 end
