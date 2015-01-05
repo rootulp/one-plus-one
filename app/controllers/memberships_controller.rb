59eeedc1ea1cd91d4b7bd59df11efabe4be0a605
Rootul Patel <rootulp@gmail.com>
2015-01-04 22:56:37 -0500

    basic styling

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -13,13 +13,16 @@ class MembershipsController < ApplicationController
 
   def destroy
     @membership.destroy
+    # to-do: come up with a clever way to return to page
+    redirect_to root_path
     # Add alerts if delete fails
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    # to-do: explicitly send params with correct keys
     def set_membership
-      @membership = Membership.find(params[:id])
+      @membership = Membership.find_by(team_id: params[:format], person_id: params[:id] )
     end
 
     def set_team
@@ -27,8 +30,8 @@ class MembershipsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    # def membership_params
-    #   params.require(:membership).permit(:person_id, :team_id)
-    # end
+    def membership_params
+      params.require(:membership).permit(:person_id, :team_id)
+    end
 
 end
