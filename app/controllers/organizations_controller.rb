faa360d7c6117eaca9c42171754b51d2fd3cd8fe
Rootul Patel <rootulp@gmail.com>
2015-01-05 07:46:55 -0500

    increment week every time generate_pairings is triggered

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,9 +4,11 @@ class OrganizationsController < ApplicationController
   def show
     @week = @organization.current_week
     @pairs = @organization.pairs_for(@week)
+    @unpaired = @organization.unpaired_for(@week)
   end
 
   def generate_pairings
+    @organization.update(week: @organization.week + 1)
     @organization.generate_pairings
     redirect_to root_path
   end
